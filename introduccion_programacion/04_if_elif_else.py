fd57e4743e16065cc541774c8917f9e687b82d51
alumnos nunoa <alumnosnunoa@inacap.cl>
2025-03-25 20:06:12 -0300

    modificaciones condicion logica

--- introduccion_programacion/04_if_elif_else.py
+++ introduccion_programacion/04_if_elif_else.py
@@ -1,9 +1,11 @@
-calificacion = 85
-if calificacion >= 90:
-    print("Excelente")
+calificacion = 55
+if calificacion > 100:
+    print("Excede límite de calificacion, ingresar hasta 100.")
+elif calificacion >= 90:
+    print("Excelente, calificacion", calificacion)
 elif calificacion >= 80:
-    print("Bueno")
+    print("Bueno, calificacion", calificacion)
 elif calificacion >= 70:
-    print("Regular")
+    print("Regular, calificacion", calificacion)
 else:
-    print("Necesitas mejorar")
+    print("Necesitas mejorar, calificacion", calificacion)
