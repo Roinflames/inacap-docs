0320dfede5450644357b81bbe8d57e414d2bde7c
roinflames <rod.reyes.s@gmail.com>
2025-04-13 16:59:55 -0400

    material semana 6, dia martes

--- introduccion_programacion/semana_6/martes/09.py
+++ introduccion_programacion/semana_6/martes/09.py
@@ -1,1 +1,12 @@
 # 🔹 Ejemplo 3 (expositivo): Devolver múltiples valores usando una tupla
+# Función que retorna una tupla con operaciones matemáticas
+def operaciones_basicas(a, b):
+    suma = a + b
+    resta = a - b
+    return (suma, resta)
+
+resultado = operaciones_basicas(10, 5)
+
+print("📦 Resultados:")
+print("Suma:", resultado[0])
+print("Resta:", resultado[1])
